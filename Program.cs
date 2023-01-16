5b2c34a755d775aebfb94b1d06f3370110cfcfb1
Ujjwal Kar <Ujjwal.Kar@clinisys.com>
2023-01-16 17:05:57 +0530

    ...

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Clinisys
+{
+    public class Q{
+
+        // public int a = 9;
+
+        public static void newMethode(params int[] a){
+            // Q d = new Q();
+            Console.WriteLine("Your static value: "+ a);
+        }
+    }
+    class Hello
+    {
+        public static void Main(String[] args){
+            Q s = new Q();
+            // s.a = 5;
+            Q.newMethode(new int[] {1,5,8});
+            Console.Write("Enter a Number: ");
+            int n = Int16.Parse(Console.ReadLine());
+            Console.WriteLine("You have entered: "+ n);
+        }
+    }
+}
---
+++ abc.cs
@@ -0,0 +1,12 @@
+// namespace Clinisys.ax
+// {
+//     class O
+//     {
+//         public static void ABX(String[] args){
+//             Console.Write("Enter a Number: ");
+//             int n = Int16.Parse(Console.ReadLine());
+//             Console.WriteLine("You have entered: "+ n);
+//         }
+//     }
+
+// }
